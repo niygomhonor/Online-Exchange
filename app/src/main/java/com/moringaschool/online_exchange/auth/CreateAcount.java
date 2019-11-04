a68cc539d1bcc43c2ade129ce486b13b28fc018a
amandinek <karambiziga@gmail.com>
2019-11-04 17:21:01 +0200

    create activities

---
+++ app/src/main/java/com/moringaschool/online_exchange/auth/CreateAcount.java
@@ -0,0 +1,16 @@
+package com.moringaschool.online_exchange.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.online_exchange.R;
+
+public class CreateAcount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acount);
+    }
+}
---
+++ app/src/main/java/com/moringaschool/online_exchange/auth/LogIn.java
@@ -0,0 +1,16 @@
+package com.moringaschool.online_exchange.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.online_exchange.R;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
