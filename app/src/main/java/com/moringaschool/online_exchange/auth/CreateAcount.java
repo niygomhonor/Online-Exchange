0638f25705708d67694841a67cdf90089beadbd4
amandinek <karambiziga@gmail.com>
2019-11-05 14:54:19 +0200

    add database

--- app/src/main/java/com/moringaschool/online_exchange/auth/CreateAcount.java
+++ app/src/main/java/com/moringaschool/online_exchange/auth/CreateAcount.java
@@ -1,16 +1,139 @@
 package com.moringaschool.online_exchange.auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.moringaschool.online_exchange.MainActivity;
 import com.moringaschool.online_exchange.R;
 
-public class CreateAcount extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateAcount extends AppCompatActivity implements View.OnClickListener  {
+
+//    public static final String TAG = CreateAccount.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private ProgressDialog mAuthProgressDialog;
+    private String mNames;
+
+
+    @BindView(R.id.button)Button mButton;
+    @BindView(R.id.name)EditText mName;
+    @BindView(R.id.email) EditText mEmail;
+    @BindView(R.id.password) EditText mPassword;
+    @BindView(R.id.verifyPassword) EditText mverifyPassword;
+    @BindView(R.id.login)TextView mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acount);
+
+        ButterKnife.bind(this);
+
+        mLogin.setOnClickListener(this);
+        mButton.setOnClickListener(this);
+
+        createAuthStateListener();
+        createAuthProgressDialog();
     }
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == mLogin) {
+            Intent intent = new Intent(CreateAcount.this, LogIn.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        if (view == mButton) {
+            createNewUser();
+        }
+
+    }
+
+    private void createNewUser() {
+//        mName = mName.getText().toString().trim();
+        final String name = mName.getText().toString().trim();
+        final String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String confirmPassword = mverifyPassword.getText().toString().trim();
+
+        boolean validName = isValidName(mName);
+        boolean validEmail = isValidEmail(email);
+//            boolean validName = isValidName(name);
+        boolean validPassword = isValidPassword(password, confirmPassword);
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAcount.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+
+    }
+
+    private boolean isValidEmail(String email) {
+        boolean isGoodEmail =
+                (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEmail.setError("Please enter a valid email address");
+            return false;
+        }
+        return isGoodEmail;
+    }
+
+    private boolean isValidName(EditText name) {
+        if (name.equals("")) {
+            mName.setError("Please enter your name");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidPassword(String password, String confirmPassword) {
+        if (password.length() < 6) {
+            mPassword.setError("password containing at least 8 characters");
+            return false;
+        } else if (!password.equals(confirmPassword)) {
+            mPassword.setError("Wrong password");
+            return false;
+        }
+        return true;
+    }
+
+
+
 }
