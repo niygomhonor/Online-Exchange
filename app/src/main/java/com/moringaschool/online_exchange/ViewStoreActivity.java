245a77e0a215652fa754431a148bd61b601d0608
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-11-05 18:20:46 +0200

    created a ViewStore Activity

---
+++ app/src/main/java/com/moringaschool/online_exchange/ViewStoreActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.online_exchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_view_store );
+    }
+}
