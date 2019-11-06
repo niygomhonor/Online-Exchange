76004ca5c781418a32933a4ec4ec4e311460245d
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-11-06 15:34:04 +0200

    the widgets images

--- app/src/main/java/com/moringaschool/online_exchange/GadgetsActivity.java
+++ app/src/main/java/com/moringaschool/online_exchange/GadgetsActivity.java
@@ -10,8 +10,8 @@ public class GadgetsActivity extends AppCompatActivity {
 
     GridView gadgetsGridView;
 
-    String[] gadgetsNames ={ "Samsung","Tecno","Iphone","Itel"};
-    int[]    gadgetsPics={ R.drawable.widg1,R.drawable.widg2,R.drawable.widg4,R.drawable.widg2};
+    String[] gadgetsNames ={ "Ipod","Headphones","Speaker","Bluetooth","Camera","Speaker","Printer","Speaker"};
+    int[]    gadgetsPics={ R.drawable.widg1,R.drawable.widg2,R.drawable.widg4,R.drawable.widg5,R.drawable.widg6,R.drawable.widg7,R.drawable.widg8};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
