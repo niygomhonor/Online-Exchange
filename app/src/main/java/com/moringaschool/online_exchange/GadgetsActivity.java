56ee7a9b9be4ccc1987dfc92a240fe4ca752d811
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-11-06 09:45:08 +0200

    added the gridview in xml

---
+++ app/src/main/java/com/moringaschool/online_exchange/GadgetsActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.online_exchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GadgetsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gadgets);
+    }
+}
