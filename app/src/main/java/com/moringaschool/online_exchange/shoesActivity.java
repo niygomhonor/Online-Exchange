0bb0987350398f29ba042c6e2070f6d56c283e26
kimbagira Sylvie <sylviekimbagira@gmail.com>
2019-11-06 13:14:53 +0200

    added changes

---
+++ app/src/main/java/com/moringaschool/online_exchange/shoesActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.online_exchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoes);
+    }
+}
